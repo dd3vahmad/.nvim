cc70f57e02d67c77a4a5722a159e6ecc6da82734
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-08-26 18:21:04 +0100

    Add: more buffer mappings to ease my life with nvim.

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -7,6 +7,10 @@ return {
       n = {
         ["<leader>ph"] = { "<cmd>Telescope neovim-project history<CR>", desc = "Project History" },
         ["<leader>pd"] = { "<cmd>Telescope neovim-project discover<CR>", desc = "Discover Projects" },
+        ["<Tab>"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
+        ["<S-Tab>"] = { "<cmd>bprevious<cr>", desc = "Previous buffer" },
+        ["<C-w>"] = { "<cmd>Bdelete<cr>", desc = "Close current buffer" },
+        ["<C-a>"] = { "ggVG", desc = "Selects all in the current buffer" },
       },
     },
   },
